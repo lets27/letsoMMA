5768e13e8c5bab901460a4dc0c6bfc6383b36a0e
LETSO27 <letsomogwera61@gmail.com>
2025-02-05 01:09:52 +0200

    updated intro.css and main jsx

--- blogInterface/src/main.jsx
+++ blogInterface/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import UserProvider from "./context/context.jsx";
-// import { UserProvider } from "./context/contextProvider.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
