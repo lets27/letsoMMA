c2aee2176601f09cc6f6e9e67d17ba6166243699
LETSO27 <letsomogwera61@gmail.com>
2025-02-05 01:53:07 +0200

    updated vite.config and set all cors access in app

--- demoRest/app.js
+++ demoRest/app.js
@@ -27,26 +27,28 @@ app.use(express.json());
 //set up non get&post converter
 app.use(methodOverride("_method"));
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://myapp.com",
-  "http://localhost:5173",
-  "http://localhost:5174",
-];
+// const allowedOrigins = [
+//   "http://localhost:3000",
+//   "https://myapp.com",
+//   "http://localhost:5173",
+//   "http://localhost:5174",
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow the request
-      } else {
-        callback(new Error("Not allowed by CORS")); // Block the request
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true); // Allow the request
+//       } else {
+//         callback(new Error("Not allowed by CORS")); // Block the request
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
+
+app.use(cors({ origin: "*" }));
 
 app.use("/public", publicRouter);
 app.use("/official", privateUsers);
